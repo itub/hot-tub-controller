c462d192965b6feea0f95884b31c4bc372862ab2
Dan Hardy <hardydb@gmail.com>
2016-11-15 20:05:48 +0000

    Add daily test text message

--- startup.py
+++ startup.py
@@ -34,47 +34,67 @@ class HotTubServer(object):
         self.last_alert = datetime.datetime.utcfromtimestamp(0)
         self.adclock = threading.Lock()
         Timer(30.0, self.filter_timer).start()
+        self.twilio_heartbeat()
 
     @cherrypy.expose
     def index(self):
         return serve_file('/home/pi/hot-tub-controller/index.html',
                           'text/html')
 
-    def filter_timer(self):
-        self.current()
-        self.config.read()
-        with open('/home/pi/filter.json') as fd:
-            filter_settings = json.loads(fd.read())
-        now = datetime.datetime.now()
-        seconds = (now.hour * 3600) + (now.minute * 60) + now.second
-        # freeze control checks
-        self.filter_status = 1 if (filter_settings['start'] <= seconds and
-              filter_settings['end'] >= seconds) else 0
-        self.freeze_status = 1 if self.status.tempAir < 42.0 else 0
-        if self.freeze_status == 1 and self.status.pump1 == 0:
-            self.controller.pump1_low()
-        elif self.filter_status == 1 and self.status.pump1 == 0:
-            self.controller.pump1_low()
-        elif self.status.pump1 == 0 and (not self.filter_status == 1) and (not self.freeze_status == 1):
-            self.controller.pump1_off()
-        if self.status.tempIn < 60.0 and self.freeze_status == 1 and \
-            (datetime.datetime.now() - self.last_alert).total_seconds() > 300:
-            print "WARNING: WATER TEMPERATURE ALERT. POSSIBLE POWER OUTAGE."
-            try:
-                with open('/home/pi/alerts.json') as fd:
-                    alerts = json.loads(fd.read())
-                if alerts['number']:
-                    client = TwilioRestClient(alerts['twilio_sid'], alerts['twilio_token'])
-                    client.messages.create(
-                        to = alerts['number'],
-                        from_ = alerts['twilio_number'],
-                        body = "WARNING - hot tub freeze alarm: {:.1f}F".format(
-                            self.status.tempIn)
+    def twilio_heartbeat(self):
+        try:
+            with open('/home/pi/alerts.json') as fd:
+                alerts = json.loads(fd.read())
+            if not alerts['number']:
+                return
+            for number in alerts['number'].split(','):
+                client = TwilioRestClient(alerts['twilio_sid'], alerts['twilio_token'])
+                client.messages.create(
+                    to = number,
+                    from_ = alerts['twilio_number'],
+                    body = "Daily hot tub text alert test."
                     )
-                self.last_alert = datetime.datetime.now()
-            except Exception as err:
-                print "Error sending SMS alert: {}".format(err)
-        Timer(30.0, self.filter_timer).start()
+        finally:
+            Timer(24 * 60 * 60, self.twilio_heartbeat).start()
+
+    def filter_timer(self):
+        try:
+            self.current()
+            self.config.read()
+            with open('/home/pi/filter.json') as fd:
+                filter_settings = json.loads(fd.read())
+            now = datetime.datetime.now()
+            seconds = (now.hour * 3600) + (now.minute * 60) + now.second
+            # freeze control checks
+            self.filter_status = 1 if (filter_settings['start'] <= seconds and
+                  filter_settings['end'] >= seconds) else 0
+            self.freeze_status = 1 if self.status.tempAir < 37.0 else 0
+            if self.freeze_status == 1 and self.status.pump1 == 0:
+                self.controller.pump1_low()
+            elif self.filter_status == 1 and self.status.pump1 == 0:
+                self.controller.pump1_low()
+            elif self.status.pump1 == 0 and (not self.filter_status == 1) and (not self.freeze_status == 1):
+                self.controller.pump1_off()
+            with open('/home/pi/alerts.json') as fd:
+                alerts = json.loads(fd.read())
+            if self.status.tempIn < alerts['threshold'] and self.freeze_status == 1 and \
+                (datetime.datetime.now() - self.last_alert).total_seconds() > 300:
+                print "WARNING: WATER TEMPERATURE ALERT. POSSIBLE POWER OUTAGE."
+                try:
+                    if alerts['number']:
+                        for number in alerts['number'].split(','):
+                            client = TwilioRestClient(alerts['twilio_sid'], alerts['twilio_token'])
+                            client.messages.create(
+                                to = number,
+                                from_ = alerts['twilio_number'],
+                                body = "WARNING - hot tub freeze alarm: {:.1f}F".format(
+                                     self.status.tempIn)
+                            )
+                    self.last_alert = datetime.datetime.now()
+                except Exception as err:
+                    print "Error sending SMS alert: {}".format(err)
+        finally:
+            Timer(30.0, self.filter_timer).start()
 
     @cherrypy.expose
     def getconfig(self):
@@ -85,8 +105,8 @@ class HotTubServer(object):
         try:
             self.adclock.acquire(True)
             self.status.tempAir = thermistor.adc_value_to_F(self.adc.readadc(7))
-            self.status.tempIn = thermistor.adc_value_to_F(self.adc.readadc(3))
-            self.status.tempOut = thermistor.adc_value_to_F(self.adc.readadc(5))
+            self.status.tempIn = thermistor.adc_value_to_F(self.adc.readadc(5))
+            self.status.tempOut = thermistor.adc_value_to_F(self.adc.readadc(3))
             status = self.status.to_jsonable()
             status['freeze_status'] = self.freeze_status
             status['filter_status'] = self.filter_status
